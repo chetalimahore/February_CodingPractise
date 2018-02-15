54140887b1c7a303ff090d36cc65c8152fd9b29c
chetalimahore <32920726+chetalimahore@users.noreply.github.com>
2018-02-15 15:26:58 -0500

    Update ClimbingStairs.cpp

--- ClimbingStairs.cpp
+++ ClimbingStairs.cpp
@@ -1,41 +1,41 @@
 #include<iostream>
 #include<string>
- 
+
 /*------Determine the number of ways to climb to the top given that you can climb only 1 or 2 steps at a time*/
 class Solution{
-	public:
-		bool check(int i){
-			for(int j= 1 ; j < sizeof(a) / sizeof(int); j++){
-				if(i == a[j])
-					return true;
-			}
-			return false; 
+public:
+	bool check(int i){
+		for(int j= 1 ; j < sizeof(a) / sizeof(int); j++){
+			if(i == a[j])
+				return true;
 		}
+		return false; 
+	}
 
-		int result(int i){ 
-			int sum = 0;
-			for(int j = 1; j < sizeof(a) / sizeof(int) ; ++j){
-				if(i - b[j] > 0){
-					sum += b[i - b[j]];
-				}
+	int result(int i){ 
+		int sum = 0;
+		for(int j = 1; j < sizeof(a) / sizeof(int) ; ++j){
+			if(i - b[j] > 0){
+				sum += b[i - b[j]];
 			}
-			return sum;
 		}
+		return sum;
+	}
 
-		int climbStairs(int n) {
-		  b = new int[n+1];
-          for(int i = 1; i <= n ; ++i){
-          	 if(check(i)){
-          	 	b[i] = 1 + result(i);
-          	 }
-          	 else b[i] = result(i);
-          }
-          return b[n];
-    }
+	int climbStairs(int n) {
+		b = new int[n+1];
+		for(int i = 1; i <= n ; ++i){
+			if(check(i)){
+				b[i] = 1 + result(i);
+			}
+			else b[i] = result(i);
+		}
+		return b[n];
+	}
 	
-	private:
-		int a[3]=  {0, 1, 2};
-		int *b;
+private:
+	int a[3]=  {0, 1, 2};
+	int *b;
 
 };
 
